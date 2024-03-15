b822a5104f70f24f363380622492365a352709cb
YingHREN <yinghaoren.cs@gmail.com>
2024-03-15 19:04:54 +0800

    do more experiments

--- tpc-ds_test/spark_create_table.py
+++ tpc-ds_test/spark_create_table.py
@@ -155,18 +155,7 @@ OPTIONS (
 
 # 关闭 SparkSession
 # result_df = spark.sql("SELECT * FROM store_returns LIMIT 10")
-sub_q1_df = spark.sql("""
-    SELECT sr_customer_sk AS ctr_customer_sk,
-           sr_store_sk AS ctr_store_sk,
-           SUM(sr_return_amt) AS ctr_total_return
-    FROM store_returns, date_dim
-    WHERE sr_returned_date_sk = d_date_sk
-          AND d_year = 2001
-    GROUP BY sr_customer_sk, sr_store_sk
-""")
-
-# q1_df = spark.sql("""
-#     WITH customer_total_return AS (
+# sub_q1_df = spark.sql("""
 #     SELECT sr_customer_sk AS ctr_customer_sk,
 #            sr_store_sk AS ctr_store_sk,
 #            SUM(sr_return_amt) AS ctr_total_return
@@ -174,21 +163,35 @@ sub_q1_df = spark.sql("""
 #     WHERE sr_returned_date_sk = d_date_sk
 #           AND d_year = 2001
 #     GROUP BY sr_customer_sk, sr_store_sk
-#     )
-#     SELECT c_customer_id
-#     FROM customer_total_return ctr1, store, customer
-#     WHERE ctr1.ctr_total_return > (
-#         SELECT AVG(ctr_total_return) * 1.2
-#         FROM customer_total_return ctr2
-#         WHERE ctr1.ctr_store_sk = ctr2.ctr_store_sk
-#     )
-#         AND ctr1.ctr_customer_sk = c_customer_sk
-#         AND s_state = 'TN'
-#     ORDER BY c_customer_id
-#     LIMIT 100
 # """)
 
+q1_df = spark.sql("""
+    WITH customer_total_return AS (
+    SELECT sr_customer_sk AS ctr_customer_sk,
+           sr_store_sk AS ctr_store_sk,
+           SUM(sr_return_amt) AS ctr_total_return
+    FROM store_returns, date_dim
+    WHERE sr_returned_date_sk = d_date_sk
+          AND d_year = 2001
+    GROUP BY sr_customer_sk, sr_store_sk
+    )
+    SELECT c_customer_id
+    FROM customer_total_return ctr1, store, customer
+    WHERE ctr1.ctr_total_return > (
+        SELECT AVG(ctr_total_return) * 1.2
+        FROM customer_total_return ctr2
+        WHERE ctr1.ctr_store_sk = ctr2.ctr_store_sk
+    )
+        AND ctr1.ctr_customer_sk = c_customer_sk
+        AND s_state = 'TN'
+    ORDER BY c_customer_id
+    LIMIT 100
+""")
+
 # 显示查询结果
-# q1_df.show()
-sub_q1_df.explain()
+q1_df.show()
+q1_df.explain()
+# sub_q1_df.explain()
+# sub_q1_df.show()
+# sub_q1_df.write.csv("output_dir", header=True)
 spark.stop()
